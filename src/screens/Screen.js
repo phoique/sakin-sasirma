d1116e6bc046d0916ba46b783e026cf38ea7fa78
Enes Kahraman <official.phoique@gmail.com>
2019-04-06 02:10:02 +0300

    Hooks func

--- src/screens/Screen.js
+++ src/screens/Screen.js
@@ -1,30 +1,20 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import Home from './Home';
 import Game from './Game';
 import EndGame from './EndGame';
 
-export default class Screen extends Component {
+export default Screen = () => {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      navigation: 'home'
-    };
-
-  }
-
-  render() {
-    console.log(this.state)
-    switch (this.state.navigation) {
-      case 'home':
-        return <Home />;
-      case 'game':
-        return <Game />;
-      case 'endgame': 
-        return <EndGame />;
-      default:
-        <Home />
-    }
+  const [navigation, setNavigation] = useState('home');
+  
+  switch (navigation) {
+    case 'home':
+      return <Home />;
+    case 'game':
+      return <Game />;
+    case 'endgame': 
+      return <EndGame />;
+    default:
+      <Home />
   }
 }
