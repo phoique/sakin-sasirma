d78aebeb66d42279ddd9587c77e96547895e7371
Enes Kahraman <official.phoique@gmail.com>
2019-04-10 01:15:03 +0300

    Color detail component

---
+++ src/components/ColorDetail.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import CustomButton from './CustomButton';
+
+export default ColorDetail = ({name, code}) => (
+  <CustomButton 
+    text= {name} 
+    textStyle = {buttonStyles.text} 
+    touchStyle = {[buttonStyles.button, {backgroundColor: code}]}
+  />
+);
+
+const buttonStyles = StyleSheet.create({
+  text: {
+    color: 'white',
+    fontSize: 22,
+  },
+
+  button: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'orange',
+    marginTop: 20,
+    borderRadius: 20,
+    height: 110,
+    width: 110,
+    marginLeft: 20,
+  },
+});
--- src/screens/Game.js
+++ src/screens/Game.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import CustomText from '../components/CustomButton';
 import CustomButton from '../components/CustomButton';
+import ColorDetail from '../components/ColorDetail';
 
 export default class Game extends Component {
 
@@ -11,51 +12,31 @@ export default class Game extends Component {
   this.state = {
     rightColor: 'Mor',
     rightColorCode: 'purple',
+    colors: ['Turuncu', 'Mavi', 'Kırmızı', 'Sarı', 'Siyah', 'Mor'],
+    colorCode: ['orange', 'blue', 'red', 'yellow', 'black', 'purple']
   };
 }
 
   render() {
+    const { rightColor, rightColorCode, colors, colorCode } = this.state;
     return (
       <View>
         <CustomText 
-          text={this.state.rightColor}
-          textStyle = {[textStyles.text, {backgroundColor: this.state.rightColorCode}]} 
+          text={rightColor}
+          textStyle = {[textStyles.text, {backgroundColor: rightColorCode}]} 
           viewStyle = {textStyles.view}
         />
-
         <View style = {styles.containerFlex}>
-          <CustomButton 
-            text='Kırmızı' 
-            textStyle = {buttonStyles.text} 
-            touchStyle = {buttonStyles.button}
-          />
-          <CustomButton 
-            text='Mavi' 
-            textStyle = {buttonStyles.text} 
-            touchStyle = {buttonStyles.button}
-          />
-          <CustomButton 
-            text='Yeşil' 
-            textStyle = {buttonStyles.text} 
-            touchStyle = {buttonStyles.button}
-          />
-          <CustomButton 
-            text='Sarı' 
-            textStyle = {buttonStyles.text} 
-            touchStyle = {buttonStyles.button}
-          />
-          <CustomButton 
-            text='Turuncu' 
-            textStyle = {buttonStyles.text} 
-            touchStyle = {buttonStyles.button}
-          />
-          <CustomButton 
-            text='Siyah' 
-            textStyle = {buttonStyles.text} 
-            touchStyle = {buttonStyles.button}
-          />
+          {
+            colors.map((color, key) => 
+              <ColorDetail 
+                key={key} 
+                name = {color} 
+                code = {colorCode[key]} 
+              />
+            )
+          }
         </View>
-
       </View>
     );
   }
@@ -85,23 +66,4 @@ const textStyles = StyleSheet.create({
   view: {
     alignItems: 'center'
   }
-});
-
-const buttonStyles = StyleSheet.create({
-  text: {
-    color: 'white',
-    fontSize: 22,
-  },
-
-  button: {
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#3498db',
-    marginTop: 20,
-    borderRadius: 20,
-    height: 110,
-    width: 110,
-    marginLeft: 20,
-  },
 });
