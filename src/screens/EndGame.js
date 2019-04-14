c2a8e591341e4cd1f2fcfbc5a6dc849a49a2341e
Enes Kahraman <official.phoique@gmail.com>
2019-04-14 23:30:46 +0300

    end game

--- src/screens/EndGame.js
+++ src/screens/EndGame.js
@@ -1,12 +1,60 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, {useContext} from 'react';
+import { View, StyleSheet } from 'react-native';
+import CustomText from '../components/CustomText';
+import CustomButton from '../components/CustomButton';
+import { action, Context } from '../store/Store';
 
-export default class EndGame extends Component {
-  render() {
-    return (
-      <View>
-        <Text> EndGame </Text>
-      </View>
-    )
-  }
+export default EndGame = () => {
+
+  const { state, dispatch } = useContext(Context);
+
+  return(
+    <View style={styles.container}>
+      <CustomText 
+        text= "Maalesef bilemedin. Bir daha ki sefere görüşürüz. :(" 
+        textStyle={textStyles.text} 
+      />
+
+      <CustomButton 
+        text='Tekrar Oyna' 
+        textStyle={buttonStyles.text} 
+        touchStyle={buttonStyles.button}
+        OnClick= {() => dispatch(action('game'))}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+const textStyles = StyleSheet.create({
+  text: {
+    marginTop: 80,
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+const buttonStyles = StyleSheet.create({
+  text: {
+    color: 'white',
+    fontSize: 24,
+  },
+
+  button: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3498db',
+    marginTop: 100,
+    borderRadius: 30,
+    height: 60,
+    width: 220
+  }
+});
